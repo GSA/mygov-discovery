4b0d5ea3d2987c1ddde1577d31dcb2bf754b8dc8
Greg Gershman <greg@shelrick.com>
2012-11-30 11:35:23 -0500

    Update db/schema.rb.

--- db/schema.rb
+++ db/schema.rb
@@ -11,40 +11,32 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121120163311) do
+ActiveRecord::Schema.define(:version => 20121120163554) do
 
-  create_table "domains", :force => true do |t|
-    t.string "hostname_reversed"
-    t.string "hostname_hash"
+  create_table "rates", :force => true do |t|
+    t.integer  "rater_id"
+    t.integer  "rateable_id"
+    t.string   "rateable_type"
+    t.integer  "stars",         :null => false
+    t.string   "dimension"
+    t.datetime "created_at",    :null => false
+    t.datetime "updated_at",    :null => false
   end
 
-  add_index "domains", ["hostname_hash"], :name => "index_domains_on_hostname_hash", :unique => true
+  add_index "rates", ["rateable_id", "rateable_type"], :name => "index_rates_on_rateable_id_and_rateable_type"
+  add_index "rates", ["rater_id"], :name => "index_rates_on_rater_id"
 
-  create_table "pages", :force => true do |t|
-    t.string  "url_hash"
-    t.string  "path"
-    t.integer "domain_id"
-    t.string  "title"
+  create_table "ratings", :force => true do |t|
+    t.integer  "user_id"
+    t.integer  "rate_id"
+    t.integer  "rateable_id"
+    t.string   "rateable_type", :limit => 32
+    t.datetime "created_at",                  :null => false
+    t.datetime "updated_at",                  :null => false
   end
 
-  add_index "pages", ["url_hash"], :name => "index_pages_on_url_hash", :unique => true
-
-  create_table "taggings", :force => true do |t|
-    t.integer  "tag_id"
-    t.integer  "taggable_id"
-    t.string   "taggable_type"
-    t.integer  "tagger_id"
-    t.string   "tagger_type"
-    t.string   "context",       :limit => 128
-    t.datetime "created_at"
-  end
-
-  add_index "taggings", ["tag_id"], :name => "index_taggings_on_tag_id"
-  add_index "taggings", ["taggable_id", "taggable_type", "context"], :name => "index_taggings_on_taggable_id_and_taggable_type_and_context"
-
-  create_table "tags", :force => true do |t|
-    t.string "name"
-  end
+  add_index "ratings", ["rate_id"], :name => "index_ratings_on_rate_id"
+  add_index "ratings", ["rateable_id", "rateable_type"], :name => "index_ratings_on_rateable_id_and_rateable_type"
 
   create_table "users", :force => true do |t|
     t.datetime "created_at", :null => false
