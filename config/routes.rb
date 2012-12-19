6861ff0807acb058b1c5b0b5f6b2d8ea0fd21609
Benjamin J. Balter <ben@balter.com>
2012-12-19 14:40:06 -0500

    better CORS support

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Discovery::Application.routes.draw do
   get '/pages/no-js', :to => 'pages#no_js', :as => "pages_no_js"  
   get '/pages/lookup', :to => 'pages#lookup', :as => "pages_lookup"
   resources :pages, :member => {:rate => :post}
+  
+  match '/pages/:id' => 'Application#cors', :via => :options
 
   match '/tags/:id' => 'tags#show'
 
@@ -10,6 +12,5 @@ Discovery::Application.routes.draw do
 
   require 'resque/server' 
   mount Resque::Server.new, :at => "/resque"
-
 
 end
