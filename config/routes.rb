980070e84b7b295020b902578ad083e2aa8802a1
Greg Gershman <greg@shelrick.com>
2012-12-28 13:35:47 -0500

    Remove lookup from routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Discovery::Application.routes.draw do
   get '/pages/no-js', :to => 'pages#no_js', :as => "pages_no_js"
-  get '/pages/lookup', :to => 'pages#lookup', :as => "pages_lookup"
   resources :pages, :only => [:index, :show, :create, :update] do
     resources :comments, :only => [:index, :create]
     resources :ratings, :only => [:create]
