a3da6142801febbc249e390f9c472e6c4307c245
Benjamin J. Balter <ben@balter.com>
2013-01-23 12:31:08 -0500

    assume json on tags controller

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,18 +1,14 @@
 class TagsController < ApplicationController
 
   def show
-   respond_to do |format|
-      format.json { render json: Page.tagged_with(params[:id]), :callback => params[:callback] }
-    end
+    render :json => Page.tagged_with(params[:id]), :callback => params[:callback]
   end
   
   def index
-    respond_to do |format|
-      if params[:q].nil?
-        format.json { head :bad_request }
-      else
-        format.json { render json: ActsAsTaggableOn::Tag.named_like( params[:q] ), :callback => params[:callback] }
-      end
+    if params[:q].nil?
+      render :json => {:status => "Error", :message => "query parameter required"}, :status => 400, :callback => params[:callback]
+    else
+      render :json => ActsAsTaggableOn::Tag.named_like( params[:q] ), :callback => params[:callback]
     end
   end
   
